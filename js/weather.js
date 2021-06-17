820b588476ccc4c8f2a82e456087f9074af589ef
name <email>
2021-06-17 18:40:29 +0800

    Site updated: 2021-06-17 18:40:28

--- js/weather.js
+++ js/weather.js
@@ -1,1 +1,14 @@
-<iframe scrolling="no" src="https://tianqiapi.com/api.php?style=ta&skin=durian" frameborder="0" width="400" height="24" allowtransparency="true"></iframe>
+
+
+    (function(a,h,g,f,e,d,c,b){b=function(){d=h.createElement(g);c=h.getElementsByTagName(g)[0];d.src=e;d.charset="utf-8";d.async=1;c.parentNode.insertBefore(d,c)};a["SeniverseWeatherWidgetObject"]=f;a[f]||(a[f]=function(){(a[f].q=a[f].q||[]).push(arguments)});a[f].l=+new Date();if(a.attachEvent){a.attachEvent("onload",b)}else{a.addEventListener("load",b,false)}}(window,document,"script","SeniverseWeatherWidget","//cdn.sencdn.com/widget2/static/js/bundle.js?t="+parseInt((new Date().getTime() / 100000000).toString(),10)));
+    window.SeniverseWeatherWidget('show', {
+      flavor: "bubble",
+      location: "WX55RYPU2SNV",
+      geolocation: true,
+      language: "auto",
+      unit: "c",
+      theme: "auto",
+      token: "b13ab283-9e5b-40b8-b75f-1aa860445275",
+      hover: "disabled",
+      container: "tp-weather-widget"
+    })
